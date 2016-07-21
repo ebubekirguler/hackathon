ef7ff58f6d3936061fec40e67bc27dd850c6e12e
EBUBEKIR GULER <ebubekirguler@ebubekirs-macbook-pro.local>
2016-07-21 09:11:34 +0300

    Initial Commit

---
+++ KuveytTurk Hackathon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KuveytTurk Hackathon
+//
+//  Created by EBUBEKIR GULER on 21/07/16.
+//  Copyright © 2016 Kuveyttürk Katılım Bankası A.Ş. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
